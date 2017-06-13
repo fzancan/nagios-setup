eca57fb3f5ce652a14fea0edf73662a931275cc7
Federico Zancan <fzancan@gmail.com>
2017-06-13 15:47:09 +0200

    Completed Nagios installation

--- nagios-installation.sh
+++ nagios-installation.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if system type is ubuntu
+SYSTEM=$(uname -a)
+
+if grep -iq ubuntu <<<$SYSTEM; then
+    echo "This script can run safely only on Ubuntu systems."
+    exit 1
+fi
+
 # https://www.digitalocean.com/community/tutorials/how-to-install-nagios-4-and-monitor-your-servers-on-ubuntu-14-04
 
 # Prepare installation
@@ -41,10 +49,23 @@ cd nagios-plugins-*
 make
 sudo make install
 
+# Install NRPE
+cd ~
+curl -L -O https://sourceforge.net/projects/nagios/files/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
+tar xvf nrpe-*.tar.gz
+cd nrpe-*
+./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
 
+make all
+sudo make install
+sudo make install-xinetd
+sudo make install-daemon-config
 
+# Use this instead of 'sudo vi /etc/xinetd.d/nrpe' to add current IP address to the only_from clause
+IP=$(hostname -I)
+cat /etc/xinetd.d/nrpe | sed "s/\(^.*only_from\s*=\s*127\.0\.0\.1\).*$/\1 $IP/" | sudo tee /etc/xinetd.d/nrpe
+sudo service xinetd restart
 
-
-
+# Nagios is now installed, we need to configure it.
 
 
