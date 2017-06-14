618e75bee5d1f27ca0b375bcd52173129d2bd9a4
Federico Zancan <fzancan@gmail.com>
2017-06-14 12:52:40 +0200

    Adapted for super-user check, removed sudo references

--- nagios-installation.sh
+++ nagios-installation.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check for root
+if [ $EUID -ne 0 ]; then
+    echo "You must be root to run this program."
+    exit 1
+fi
+
+
 # Check if system type is ubuntu
 SYSTEM=$(uname -a)
 grep -iq ubuntu <<<$SYSTEM
@@ -11,18 +18,18 @@ fi
 # https://www.digitalocean.com/community/tutorials/how-to-install-nagios-4-and-monitor-your-servers-on-ubuntu-14-04
 
 # Prepare installation
-sudo apt-get update
+apt-get update
 
-sudo apt install mysql-server
-sudo apt install apache2
-sudo apt install php7.0 libapache2-mod-php7.0
-sudo apt install postfix
+apt install mysql-server
+apt install apache2
+apt install php7.0 libapache2-mod-php7.0
+apt install postfix
 
-sudo useradd nagios
-sudo groupadd nagcmd
-sudo usermod -a -G nagcmd nagios
+useradd nagios
+groupadd nagcmd
+usermod -a -G nagcmd nagios
 
-sudo apt-get install build-essential libgd2-xpm-dev openssl libssl-dev xinetd apache2-utils unzip
+apt-get install build-essential libgd2-xpm-dev openssl libssl-dev xinetd apache2-utils unzip
 
 
 # Install Nagios core
@@ -32,11 +39,11 @@ tar xvf nagios-*.tar.gz
 cd nagios-*
 ./configure --with-nagios-group=nagios --with-command-group=nagcmd --with-mail=/usr/sbin/sendmail
 make all
-sudo make install
-sudo make install-commandmode
-sudo make install-init
-sudo make install-config
-sudo /usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-available/nagios.conf
+make install
+make install-commandmode
+make install-init
+make install-config
+/usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-available/nagios.conf
 
 
 # Install Nagios Plugins
@@ -47,7 +54,7 @@ cd nagios-plugins-*
 
 ./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
 make
-sudo make install
+make install
 
 # Install NRPE
 cd ~
@@ -57,18 +64,24 @@ cd nrpe-*
 ./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
 
 make all
-sudo make install
-sudo make install-xinetd
-sudo make install-daemon-config
+make install
+make install-xinetd
+make install-daemon-config
 
-# Use this instead of 'sudo vi /etc/xinetd.d/nrpe' to add current IP address to the only_from clause
+# Use this instead of 'vi /etc/xinetd.d/nrpe' to add current IP address to the only_from clause
 IP=$(hostname -I)
-cat /etc/xinetd.d/nrpe | sed "s/\(^.*only_from\s*=\s*127\.0\.0\.1\).*$/\1 $IP/" | sudo tee /etc/xinetd.d/nrpe
-sudo service xinetd restart
+cat /etc/xinetd.d/nrpe | sed "s/\(^.*only_from\s*=\s*127\.0\.0\.1\).*$/\1 $IP/" | tee /etc/xinetd.d/nrpe
+service xinetd restart
 
 # Nagios is now installed, we need to configure it.
 
-sudo vi /usr/local/nagios/etc/nagios.cfg
-cat /usr/local/nagios/etc/nagios.cfg | sed "s/#\(cfg_dir=\/usr\/local\/nagios\/etc\/servers\)/\1/" | sudo tee /usr/local/nagios/etc/nagios.cfg
+vi /usr/local/nagios/etc/nagios.cfg
+cat /usr/local/nagios/etc/nagios.cfg | sed "s/#\(cfg_dir=\/usr\/local\/nagios\/etc\/servers\)/\1/" | tee /usr/local/nagios/etc/nagios.cfg
 
+mkdir /usr/local/nagios/etc/servers
 
+# TODO: parametrize admin email contact
+ADMIN_EMAIL="federico@emailchef.com"
+cat /usr/local/nagios/etc/objects/contacts.cfg | sed "s/\(^.*email\s*\)nagios@localhost\(.*$\)/\1$ADMIN_EMAIL\2/" | tee /usr/local/nagios/etc/objects/contacts.cfg
+
+printf "\ndefine command{\n    command_name check_nrpe\n    command_line \$USER1$/check_nrpe -H \$HOSTADDRESS$ -c \$ARG1$\n}" > /usr/local/nagios/etc/objects/commands.cfg 
